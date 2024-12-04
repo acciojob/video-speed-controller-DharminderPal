beace58af631467934d0ee9c38235f3a2fed02b2
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-04 17:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const playButton = document.querySelector('.player__button');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackSpeedSlider = document.querySelector('input[name="playbackSpeed"]');
+const skipButtons = document.querySelectorAll('.skip');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playButton.textContent = '❚ ❚';
+    } else {
+        video.pause();
+        playButton.textContent = '►';
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateVolume() {
+    video.volume = volumeSlider.value;
+}
+
+function updatePlaybackSpeed() {
+    video.playbackRate = playbackSpeedSlider.value;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function updateProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+playButton.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('input', updateVolume);
+playbackSpeedSlider.addEventListener('input', updatePlaybackSpeed);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+video.addEventListener('timeupdate', updateProgress);
+progress.addEventListener('click', scrub);
